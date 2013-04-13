403df97451187ac73ec74cb3c9db0e0a97846018
Andrew Metcalf <andrew@Andrews-MacBook-Pro-2.local>
2013-04-13 12:31:43 +0300

    TEST Pre-submission commit for Fair and Square-small-000_000

--- source/Fair and Square/fair_and_square.py
+++ source/Fair and Square/fair_and_square.py
@@ -1,6 +1,31 @@
+import sys
+import math
+
+def is_palindrome(integer):
+    strval = str(int(integer))
+    return strval == strval[::-1]
+
+def solution(infilename, outfilename):
+    infile = open(infilename, "r")
+    outfile = open(outfilename, "w")
+
+    ranges = infile.read().strip().split("\n")[1:]
+    
+    for i, rng in enumerate(ranges):
+        start, end = rng.split(" ")
+        cnt = 0
+        for possible in range(int(math.ceil(math.sqrt(float(start)))), int(math.sqrt(float(end)))+1):
+            if is_palindrome(possible) and is_palindrome(possible**2):
+                cnt += 1
+        outval = "Case #%d: %d"%(i+1, cnt)
+        print outval
+        outfile.write(outval)
+        outfile.write("\n")
+
+    return True
 
 def main():
-    pass
+    solution(sys.argv[1], sys.argv[2])
 
 if __name__ == '__main__':
   main()
